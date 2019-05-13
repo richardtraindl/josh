e1cb7d47ab85fa926409e67dc47144ca8849e1eb
richardtraindl <richardtraindl@gmail.com>
2019-05-13 20:03:05 +0200

    changes

--- Procfile
+++ Procfile
@@ -1,4 +1,2 @@
 
 web: gunicorn "josh:create_app()"
-#web: gunicorn josh:app
-#web: gunicorn wsgi --log-file -
--- josh/__init__.py
+++ josh/__init__.py
@@ -7,10 +7,10 @@ from flask import Flask
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
+    """app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE_URL="postgres://postgres:postgres@localhost/joshdb",
-    )
+    )"""
 
     if(test_config is None):
         # load the instance config, if it exists, when not testing
