6c7006a7e46344dbdf1b973563a281b95afc72d7
Richard Traindl <richardtraindl@gmail.com>
2019-06-12 13:57:54 +0200

    Delete touch.py

--- josh/engine3/pieces/touch.py
+++
@@ -1,9 +0,0 @@
-class cTouch:
-    def __init__(self, piece, fieldx, fieldy):
-        self.piece = piece
-        self.fieldx = fieldx
-        self.fieldy = fieldy
-        self.attacker_beyond = []
-        self.supporter_beyond = []
-
-
