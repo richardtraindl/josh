6bef2059dbdb7163ac192f0b6db388ee16182d2b
Richard Traindl <richardtraindl@gmail.com>
2019-07-01 15:24:04 +0200

    Update helper.cpp

--- josh/cengine/helper.cpp
+++ josh/cengine/helper.cpp
@@ -8,26 +8,22 @@ typedef map<string, int> MapStrInt;
 typedef map<int, int> MapIntInt;
 typedef map<int, string> MapIntStr;
 
-def reverse_lookup(dic, value):
-    for key in dic:
-        if dic[key] == value:
-            return key
-    return None
+int coord_to_index(coord){
+    if(ord(coord[0]) > ord('H')){
+        x = ord(coord[0]) - ord('a');
+    }
+    else{
+        x = ord(coord[0]) - ord('A');
+    }
+    y = ord(coord[1]) - ord('1');
+    return (x + y * 8);
+};
 
-
-def coord_to_index(coord):
-    if(ord(coord[0]) > ord('H')):
-        x = ord(coord[0]) - ord('a')
-    else:
-        x = ord(coord[0]) - ord('A')
-    y = ord(coord[1]) - ord('1')
-    return (x + y * 8)
-
-
-def index_to_coord(idx):
-    x = idx % 8 
-    y = idx // 8
-    col = chr(x + ord('a'))
-    row = chr(y + ord('1'))
-    coord = str(col + row)
-    return coord
+int index_to_coord(idx){
+    x = idx % 8;
+    y = idx // 8;
+    col = chr(x + ord('a'));
+    row = chr(y + ord('1'));
+    coord = str(col + row);
+    return coord;
+};
